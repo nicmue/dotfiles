76b0c1c0600cff683df18b43ee25164d4ab2729e
Nico Muerdter <nico@everest-erp.com>
2025-08-20 10:52:01 +0200

    Remove .config/fish/functions/rust-analyzer.fish

--- private_dot_config/private_fish/functions/rust-analyzer.fish
+++
@@ -1,9 +0,0 @@
-function ra-update -d "Downloads the latest rust-analyer binary"
-    mkdir -p $HOME/.local/bin
-    curl -L https://github.com/rust-lang/rust-analyzer/releases/latest/download/rust-analyzer-aarch64-apple-darwin.gz  | gunzip -c - > $HOME/.local/bin/rust-analyzer
-    chmod +x $HOME/.local/bin/rust-analyzer
-end
-
-function ra-remove -d "Removes the rust-analyzer binary"
-    rm -rf $HOME/.local/bin/rust-analyzer
-end
