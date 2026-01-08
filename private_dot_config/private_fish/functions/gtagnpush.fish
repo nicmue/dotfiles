3ce573b77eb2707e73ee40ab1a0bc570062eeeec
Nico Muerdter <nico@everest-erp.com>
2026-01-08 08:46:27 +0100

    Remove .config/fish/functions/gtagnpush.fish Remove .config/fish/functions/rust-analyzer-remove.fish Remove .config/fish/functions/rust-analyzer-update.fish

--- private_dot_config/private_fish/functions/gtagnpush.fish
+++
@@ -1,4 +0,0 @@
-function gtagnpush --argument-names tag -d "Create a git tag and push it to origin"
-    git tag -a $tag -m $tag
-    git push origin $tag
-end
--- private_dot_config/private_fish/functions/rust-analyzer-remove.fish
+++
@@ -1,5 +0,0 @@
-
-function rust-analyzer-remove -d "Removes the rust-analyzer binary"
-    set fish_trace 1
-    rm $HOME/.local/bin/rust-analyzer
-end
